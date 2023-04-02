4fec89fb8f27a8fd1e37ca5fa69273db1628c63e
Raul Ruiz <raul.ruiz.barea@gmail.com>
2023-04-02 14:03:24 +0200

    infra: monorepo with api-gateway

--- nestjs-security-rest-api/apps/nestjs-security-rest-api/src/main.ts
+++ nestjs-security-rest-api/apps/nestjs-security-rest-api/src/main.ts
@@ -7,9 +7,14 @@ async function bootstrap() {
   // whitelist: true => avoid other properties than the DTO expected
   app.useGlobalPipes(
     new ValidationPipe({
+      //* properties that don't use any validator decorator automatically removed and throw an exception
       whitelist: true,
       forbidNonWhitelisted: true,
+      //* transform payload objects to dto
       transform: true,
+      //! PROD: Don't give hints about the errors
+      //TODO: Do we need this?
+      //disableErrorMessages: true
     }),
   );
   await app.listen(3000);
