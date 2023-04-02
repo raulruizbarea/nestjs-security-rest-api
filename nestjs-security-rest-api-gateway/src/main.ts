0cfa698977d61ce54200cc4eda4bf554994a0fd0
Raul Ruiz <raul.ruiz.barea@gmail.com>
2023-04-03 00:49:05 +0200

    feat: add api gateway project

---
+++ nestjs-security-rest-api-gateway/src/main.ts
@@ -0,0 +1,33 @@
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+
+import { ValidationPipe } from '@nestjs/common';
+import { NestFactory } from '@nestjs/core';
+import { ApiGatewayModule } from './api-gateway.module';
+
+async function bootstrap() {
+  const app = await NestFactory.create(ApiGatewayModule);
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      //* properties that don't use any validator decorator automatically removed and throw an exception
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      //* transform payload objects to dto
+      transform: true,
+      //! PROD: Don't give hints about the errors
+      //TODO: Do we need this?
+      //disableErrorMessages: true
+    }),
+  );
+
+  const config = new DocumentBuilder()
+    .setTitle('NestJS Security REST API')
+    .setDescription('Open University of Catalonia')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
+  await app.listen(3000);
+}
+bootstrap();
